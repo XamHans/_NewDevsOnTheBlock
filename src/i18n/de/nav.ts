9af23037e3fcbb5cfecac892b2101c656f097374
Xamhans <muellerjohannes93@gmail.com>
2023-05-06 20:24:38 +0200

    started with basic course

--- src/i18n/de/nav.ts
+++ src/i18n/de/nav.ts
@@ -6,6 +6,6 @@ export default NavDictionary({
 
 	// Core Concepts
 	coreConcepts: 'Kernkonzepte',
-	'core-concepts/1-0_structure': 'Aufbau dieses Kurses',
-	'core-concepts/1-1_stateful-web': 'Web3 - Das Web mit Zustand',
+	'basis/1-0_structure': 'Aufbau dieses Kurses',
+	'basis/1-1_stateful-web': 'Web3 - Das Web mit Zustand',
 });
--- src/i18n/en/nav.ts
+++ src/i18n/en/nav.ts
@@ -9,118 +9,30 @@
  */
 export default [
 	{ text: 'Getting Started', header: false, slug: 'getting-started', key: 'getting-started' },
-	// { text: 'Installation', slug: 'install/auto', key: 'install' },
-	// { text: 'Editor Scheetup', slug: 'editor-setup', key: 'editor-setup' },
-	// { text: 'Upgrade to v2', slug: 'guides/upgrade-to/v2', key: 'guides/upgrade-to/v2' },
 
-	{ text: 'Core Concepts', header: true, type: 'learn', key: 'coreConcepts' },
-	// { text: 'Why Astro', slug: 'concepts/why-astro', key: 'concepts/why-astro' },
-	// { text: 'MPA vs. SPA', slug: 'concepts/mpa-vs-spa', key: 'concepts/mpa-vs-spa' },
-	// { text: 'Astro Islands', slug: 'concepts/islands', key: 'concepts/islands' },
-
-	// { text: 'Tutorials', header: true, type: 'learn', key: 'tutorials' },
-	// { text: 'Build a Blog', slug: 'tutorial/0-introduction', key: 'blog-tutorial' },
-	// { text: 'Thinking with Islands', slug: 'tutorial/0-introduction', key: 'island-tutorial' },
+	{ text: 'Basis', header: true, type: 'learn', key: 'basis' },
 
 	{
 		text: 'Structure of this course',
-		slug: 'core-concepts/1-0_structure',
-		key: 'core-concepts/1-0_structure',
+		slug: 'basis/1-0_structure',
+		key: 'basis/1-0_structure',
 	},
 
 	{
-		text: 'Web3 - The Stateful Web',
-		slug: 'core-concepts/1-1_stateful-web',
-		key: 'core-concepts/1-1_stateful-web',
+		text: 'How it started - Problem first',
+		slug: 'basis/1-1_how_it_started',
+		key: 'basis/1-1_how_it_started',
 	},
 
-	// { text: 'Recipes', header: true, type: 'learn', key: 'examples' },
-	// { text: 'Migrate to Astro', slug: 'guides/migrate-to-astro', key: 'guides/migrate-to-astro' },
-	// { text: 'Connect a CMS', slug: 'guides/cms', key: 'guides/cms' },
-	// { text: 'Add integrations', slug: 'guides/integrations-guide', key: 'guides/integrations-guide' },
-	// { text: 'Deploy your site', slug: 'guides/deploy', key: 'guides/deploy' },
-	// { text: 'More Recipes', slug: 'recipes', key: 'guides/recipes' },
+	{
+		text: 'Bitcoin - To the rescue',
+		slug: 'basis/1-2_bitcoin',
+		key: 'basis/1-2_bitcoin',
+	},
 
-	// { text: 'Guides', header: true, type: 'learn', key: 'features' },
-	// {
-	// 	text: 'Astro Template Syntax',
-	// 	slug: 'core-concepts/astro-syntax',
-	// 	key: 'core-concepts/astro-syntax',
-	// },
-	// {
-	// 	text: 'UI Frameworks',
-	// 	slug: 'core-concepts/framework-components',
-	// 	key: 'core-concepts/framework-components',
-	// },
-	// { text: 'Routing', slug: 'core-concepts/routing', key: 'core-concepts/routing' },
-	// { text: 'Markdown & MDX', slug: 'guides/markdown-content', key: 'guides/markdown-content' },
-	// {
-	// 	text: 'Content Collections',
-	// 	slug: 'guides/content-collections',
-	// 	key: 'guides/content-collections',
-	// },
-	// {
-	// 	text: 'Scripts & Event Handling',
-	// 	slug: 'guides/client-side-scripts',
-	// 	key: 'guides/client-side-scripts',
-	// },
-	// { text: 'CSS & Styling', slug: 'guides/styling', key: 'guides/styling' },
-	// { text: 'Assets (Experimental)', slug: 'guides/assets', key: 'guides/assets' },
-	// { text: 'Images', slug: 'guides/images', key: 'guides/images' },
-	// { text: 'Fonts', slug: 'guides/fonts', key: 'guides/fonts' },
-	// { text: 'Imports', slug: 'guides/imports', key: 'guides/imports' },
-	// {
-	// 	text: 'Server-side Rendering (SSR)',
-	// 	slug: 'guides/server-side-rendering',
-	// 	key: 'guides/server-side-rendering',
-	// },
-	// { text: 'Endpoints', slug: 'core-concepts/endpoints', key: 'core-concepts/endpoints' },
-	// { text: 'Data Fetching', slug: 'guides/data-fetching', key: 'guides/data-fetching' },
-	// { text: 'Testing', slug: 'guides/testing', key: 'guides/testing' },
-	// { text: 'Troubleshooting', slug: 'guides/troubleshooting', key: 'guides/troubleshooting' },
-
-	// { text: 'Configuration', header: true, type: 'learn', key: 'configuration' },
-	// {
-	// 	text: 'The Astro Config File',
-	// 	slug: 'guides/configuring-astro',
-	// 	key: 'guides/configuring-astro',
-	// },
-	// { text: 'TypeScript', slug: 'guides/typescript', key: 'guides/typescript' },
-	// { text: 'Import Aliases', slug: 'guides/aliases', key: 'guides/aliases' },
-	// {
-	// 	text: 'Environment Variables',
-	// 	slug: 'guides/environment-variables',
-	// 	key: 'guides/environment-variables',
-	// },
-
-	// { text: 'Reference', header: true, type: 'api', key: 'reference' },
-	// {
-	// 	text: 'Configuration',
-	// 	slug: 'reference/configuration-reference',
-	// 	key: 'reference/configuration-reference',
-	// },
-	// { text: 'Runtime API', slug: 'reference/api-reference', key: 'reference/api-reference' },
-	// {
-	// 	text: 'Integrations API',
-	// 	slug: 'reference/integrations-reference',
-	// 	key: 'reference/integrations-reference',
-	// },
-	// { text: 'Adapter API', slug: 'reference/adapter-reference', key: 'reference/adapter-reference' },
-	// {
-	// 	text: 'Image Service API',
-	// 	slug: 'reference/image-service-reference',
-	// 	key: 'reference/image-service-reference',
-	// },
-	// {
-	// 	text: 'Template Directives',
-	// 	slug: 'reference/directives-reference',
-	// 	key: 'reference/directives-reference',
-	// },
-	// { text: 'The Astro CLI', slug: 'reference/cli-reference', key: 'reference/cli-reference' },
-	// {
-	// 	text: 'Error Reference',
-	// 	slug: 'reference/error-reference',
-	// 	key: 'reference/error-reference',
-	// },
-	// { text: 'NPM Package Format', slug: 'reference/publish-to-npm', key: 'guides/publish-to-npm' },
+	{
+		text: 'Blockchain - the fundamental technology',
+		slug: 'basis/1-3_blockchain',
+		key: 'basis/1-3_blockchain',
+	},
 ] as const;
